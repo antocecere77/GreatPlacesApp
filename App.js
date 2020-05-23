2a90d2d496e437a820e30f1f2e8600e024678197
antocecere <antocecere77@gmail.com>
2020-05-23 14:22:21 +0200

    Redux & adding places

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <PlacesNavigator />
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
   );
 }
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, ScrollView, StyleSheet} from 'react-native';
 
+import { useDispatch } from 'react-redux';
+
 import Colors from '../costants/Colors';
+import * as placesActions from '../store/places-action';
 
 const NewPlaceScreen = props => {
 
     const [titleValue, setTitleValue] = useState('');
+    const dispatch = useDispatch();
 
     const titleChangeHandler = text => {
         setTitleValue(text);
     }
 
     const savePlaceHandler = () => {
-        
+        dispatch(placesActions.addPlace(titleValue));
+        props.navigation.goBack();
     };
 
     return (
