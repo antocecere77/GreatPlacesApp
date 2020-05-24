f50b4343fcd065e31ae1caad801ab487dcff5c1c
antocecere <antocecere77@gmail.com>
2020-05-24 12:32:22 +0200

    Configuring the camera access

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Button, Image, Text, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
@@ -6,12 +6,13 @@ import * as Permissions from 'expo-permissions';
 import Colors from '../costants/Colors';
 
 const ImgPicker = props => {
-
+    const [pickedImage, setPickedImage] = useState();
+    
     const verifyPermissions = async () => {
-       const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+       const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL );
        if(result.status !== 'granted') {
            Alert.alert(
-                'Insfficient permissins!', 
+                'Insufficient permissions!', 
                 'You need to grant camera permissions to use this app', 
                 [{test: 'Okay'}]);
             return false;
@@ -24,13 +25,20 @@ const ImgPicker = props => {
         if(!hasPermission) {
             return;
         }
-        ImagePicker.launchCameraAsync();
+        const image = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [16, 9],
+            quality: 0.5
+        });
+
+        setPickedImage(image.uri);
     };
 
     return <View style={styles.imagePicker}>
               <View style={styles.imagePreview}>
-                  <Text>No image picked yet.</Text>
-                  <Image style={styles.image}/>
+                  {!pickedImage
+                        ? <Text>No image picked yet.</Text> 
+                        : <Image style={styles.image} source={{uri: pickedImage}}/>}                  
               </View>  
               <Button title="Take Image" color={Colors.primary} onPress={takeImageHandler}/>
            </View>
